8a96f08129772ff3f3ccc30fee5605f4f647221f
eschabell <eschabell@16b9798a-5803-0410-9976-a82bee9f84b4>
2006-04-02 20:20:33 +0000

    First AbTLinux class file being added to play with for ruby doc generation testing.

---
+++ src/trunk/Package.rb
@@ -0,0 +1,41 @@
+#!/usr/bin/ruby -w
+
+##
+# Package.rb 
+#
+# Package class provides an interface to package creation within AbTLinux. By
+# inheriting from this class (class Fortune < Package) one picks up all
+# supported standard functions for the abt package manager to make use of the
+# new package.
+#
+# Created by Eric D. Schabell <erics@abtlinux.org>
+# Copyright 2006, GPL.
+#
+# This file is part of AbTLinux.
+#
+# AbTLinux is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# AbTLinux is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#																    
+# You should have received a copy of the GNU General Public License along with
+# AbTLinux; if not, write to the Free Software Foundation, Inc., 51 Franklin
+# St, Fifth Floor, Boston, MA 02110-1301  USA
+##
+
+class Package
+
+	protected
+
+	def details
+	end
+	
+	def pre
+	end
+
+end
