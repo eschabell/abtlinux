ba91885aeaab8bdb9160cc3dcd0f9dbf06633f0f
eschabell <eschabell@16b9798a-5803-0410-9976-a82bee9f84b4>
2006-10-14 13:26:15 +0000

    Added the scenarios from section 3.3 options. Overview of available options:
    Usage: abt.rb [options]
    
    packages:
      -i,  install     [package]            Install given package.
      -ri, reinstall   [package]            Reinstall given package.
      -r,  remove      [package]            Remove given package.
      -dg, downgrade   [version] [package]  Downgrade given package to given version.
      -f,  freeze      [package]            Holds given package at current version, prevents upgrades.
    
    queries:
      -s,  search      [string | regexp ]   Search package descriptions for given input.
      show-details     [package]            Show give package details.
      show-build       [package]            Show build log of given package.
      show-depends     [package]            Show the dependency tree of given package.
      show-files       [package]            Show all installed files from given package.
      show-owner       [file]               Show the package owning given file.
      show-installed                        Show list of all installed packages.
      show-frozen                           Show list of all frozen packages.
      show-untracked                        Show all files on system not tracked by AbTLinux.
      show-journal                          Show the system journal.
      show-iqueue                           Show the contents of the install queue.
      show-patches                          Show the current available patches for installed package tree.
    
    generation:
      show-updates          Show a package listing with available update versions.
      html                  Generate HTML page from installed packages:
                                    (package name with hyperlink to package website and version installed)

--- src/trunk/abt.rb
+++ src/trunk/abt.rb
@@ -33,25 +33,33 @@ class AbtUsage
 
     def usage
         puts "Usage: abt.rb [options]\n\n"
+        
+        puts "packages:"
 		puts "  -i,  install     [package]\t\tInstall given package."
 		puts "  -ri, reinstall   [package]\t\tReinstall given package."
 		puts "  -r,  remove      [package]\t\tRemove given package."
 		puts "  -dg, downgrade   [version] [package]\tDowngrade given package to given version."
 		puts "  -f,  freeze      [package]\t\tHolds given package at current version, prevents upgrades."
-		puts "  -s,  search      [string | regexp ]\tSearch package descriptions for given input."
 		puts
+		puts "queries:"
+		puts "  -s,  search      [string | regexp ]\tSearch package descriptions for given input."
 		puts "  show-details     [package]\t\tShow give package details."
 		puts "  show-build       [package]\t\tShow build log of given package."
 		puts "  show-depends     [package]\t\tShow the dependency tree of given package."
 		puts "  show-files       [package]\t\tShow all installed files from given package."
 		puts "  show-owner       [file]\t\tShow the package owning given file."
-        puts
         puts "  show-installed\t\t\tShow list of all installed packages."
 		puts "  show-frozen\t\t\t\tShow list of all frozen packages."
 		puts "  show-untracked\t\t\tShow all files on system not tracked by AbTLinux."
 		puts "  show-journal\t\t\t\tShow the system journal."
 		puts "  show-iqueue\t\t\t\tShow the contents of the install queue."
 		puts "  show-patches\t\t\t\tShow the current available patches for installed package tree."
+		puts
+		puts "generation:"
+		puts "  show-updates\t\tShow a package listing with available update versions."
+		puts "  html\t\t\tGenerate HTML page from installed packages:"
+        puts "  \t\t\t\t(package name with hyperlink to package website and version installed)"
+ 
 	end
 end
 
@@ -187,6 +195,12 @@ end
     when "show-patches"  
 		puts "Display currently available patches for installed package tree."
 
+    when "show-updates"  
+		puts "Display package listing with available update versions."
+
+    when "html"
+        puts "Generate HTML page from installed packages:"
+        puts "  (package name with hyperlink to package website and version installed)"
     end # case
 	
 	#puts 'DEBUG: options are -'
