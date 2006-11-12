60b3ea628b2c0b5f4c0334136a2b8bc675b01419
eschabell <eschabell@16b9798a-5803-0410-9976-a82bee9f84b4>
2006-11-12 17:32:49 +0000

    Cleanup and beautification of the code layout.

--- src/trunk/abt.rb
+++ src/trunk/abt.rb
@@ -31,10 +31,9 @@ require 'optparse'
 
 class AbtUsage
 
-    def usage
-        puts "Usage: abt.rb [options]\n\n"
-        
-        puts "packages:"
+	def usage
+		puts "Usage: abt.rb [options]\n\n"
+    puts "packages:"
 		puts "  -i,  install     [package]\t\tInstall given package."
 		puts "  -ri, reinstall   [package]\t\tReinstall given package."
 		puts "  -r,  remove      [package]\t\tRemove given package."
@@ -48,7 +47,7 @@ class AbtUsage
 		puts "  show-depends     [package]\t\tShow the dependency tree of given package."
 		puts "  show-files       [package]\t\tShow all installed files from given package."
 		puts "  show-owner       [file]\t\tShow the package owning given file."
-        puts "  show-installed\t\t\tShow list of all installed packages."
+    puts "  show-installed\t\t\tShow list of all installed packages."
 		puts "  show-frozen\t\t\t\tShow list of all frozen packages."
 		puts "  show-untracked\t\t\tShow all files on system not tracked by AbTLinux."
 		puts "  show-journal\t\t\t\tShow the system journal."
@@ -58,28 +57,28 @@ class AbtUsage
 		puts "generation:"
 		puts "  show-updates\t\tShow a package listing with available update versions."
 		puts "  html\t\t\tGenerate HTML page from installed packages:"
-        puts "  \t\t\t\t(package name with hyperlink to package website and version installed)"
-        puts
-        puts "downloads:"
-        puts "  -d,  download     [package]\t\tRetrieve given package sources."
-        puts "  -u,  update       [package]|[tree]\tUpdate given package or tree from AbTLinux repository."
-        puts "  -n,  news\t\t\t\tDisplays newsfeed from AbTLinux website."
-        puts
-        puts "fix:"
-        puts "  purge-src\t\t\t\tRemove source caches for packages no longer installed."
-        puts "  purge-logs\t\t\t\tRemove log files for packages no longer installed."
-        puts "  verify-files      [package]\t\tInstalled files are verified for given package."
-        puts "  verify-symlinks   [package]\t\tSymlinks verified for given package."
-        puts "  verify-deps       [package]\t\tDependency tree is verified for given package."
-        puts "  verify-integrity  [package]\t\tVerify integrity of installed files for given package."
-        puts "  fix               [package]\t\tGiven package is verified and fixed if needed."
-        puts
-        puts "maintenance:"
-        puts "  build-location    [host]\t\tSets global location (default: localhost) for retrieving cached package builds."
-        puts "  package-repo      [add|remove|list] [URI]"
-        puts "                                        add    - add package repository to list."
-        puts "                                        remove - remove a package repository from list."
-        puts "                                        list   - display current repository list."
+    puts "  \t\t\t\t(package name with hyperlink to package website and version installed)"
+    puts
+    puts "downloads:"
+    puts "  -d,  download     [package]\t\tRetrieve given package sources."
+    puts "  -u,  update       [package]|[tree]\tUpdate given package or tree from AbTLinux repository."
+    puts "  -n,  news\t\t\t\tDisplays newsfeed from AbTLinux website."
+    puts
+    puts "fix:"
+    puts "  purge-src\t\t\t\tRemove source caches for packages no longer installed."
+    puts "  purge-logs\t\t\t\tRemove log files for packages no longer installed."
+    puts "  verify-files      [package]\t\tInstalled files are verified for given package."
+    puts "  verify-symlinks   [package]\t\tSymlinks verified for given package."
+    puts "  verify-deps       [package]\t\tDependency tree is verified for given package."
+    puts "  verify-integrity  [package]\t\tVerify integrity of installed files for given package."
+    puts "  fix               [package]\t\tGiven package is verified and fixed if needed."
+    puts
+    puts "maintenance:"
+    puts "  build-location    [host]\t\tSets global location (default: localhost) for retrieving cached package builds."
+    puts "  package-repo      [add|remove|list] [URI]"
+    puts "                                        add    - add package repository to list."
+    puts "                                        remove - remove a package repository from list."
+    puts "                                        list   - display current repository list."
 	end
 end
 
@@ -94,253 +93,251 @@ if ( ARGV.length == 0 )
 end
 
 case ARGV[0]
+
+	when "install", "-i"
+		if ( ARGV.length == 2 )
+			options['package'] = ARGV[1]
+			puts "Installing package : " + options['package']
+		else
+			show.usage
+			exit
+		end
 	
-when "install", "-i"
-	if ( ARGV.length == 2 )
-		options['package'] = ARGV[1]
-		puts "Installing package : " + options['package']
-	else
-		show.usage
-		exit
-	end
+	when "reinstall", "-ri"
+		if ( ARGV.length == 2 )
+			options['package'] = ARGV[1]
+			puts "Reinstalling package : " + options['package']
+		else
+			show.usage
+			exit
+		end
 	
+	when "remove", "-r"
+		if ( ARGV.length == 2 )
+			options['package'] = ARGV[1]
+			puts "Removing package : " + options['package']
+		else
+			show.usage
+			exit
+		end
 	
-when "reinstall", "-ri"
-	if ( ARGV.length == 2 )
-		options['package'] = ARGV[1]
-		puts "Reinstalling package : " + options['package']
-	else
-		show.usage
-		exit
-	end
+	when "downgrade", "-dg"
+		if ( ARGV.length == 3 )
+		    options['version'] = ARGV[1]
+			options['package'] = ARGV[2]
+			puts "Downgradinging package : " + options['package'] + " to version : " + options['version']
+		else
+			show.usage
+			exit
+		end
 	
-when "remove", "-r"
-	if ( ARGV.length == 2 )
-		options['package'] = ARGV[1]
-		puts "Removing package : " + options['package']
-	else
-		show.usage
-		exit
-	end
+	when "freeze", "-f"  
+		if ( ARGV.length == 2 )
+			options['package'] = ARGV[1]
+			puts "Holdinging package : " + options['package'] + " at the current version."
+		else
+			show.usage
+			exit
+		end
+		
+	when "search", "-s"  
+		if ( ARGV.length == 2 )
+			options['searchString'] = ARGV[1]
+			puts "Searching package descriptions for : " + options['searchString']
+		else
+			show.usage
+			exit
+		end
 	
-when "downgrade", "-dg"
-	if ( ARGV.length == 3 )
-	    options['version'] = ARGV[1]
-		options['package'] = ARGV[2]
-		puts "Downgradinging package : " + options['package'] + " to version : " + options['version']
-	else
-		show.usage
-		exit
-	end
+	when "show-details"  
+		if ( ARGV.length == 2 )
+			options['package'] = ARGV[1]
+			puts "Display details for package : " + options['package']
+		else
+			show.usage
+			exit
+		end
 	
-when "freeze", "-f"  
-	if ( ARGV.length == 2 )
-		options['package'] = ARGV[1]
-		puts "Holdinging package : " + options['package'] + " at the current version."
-	else
-		show.usage
-		exit
-	end
-	
-when "search", "-s"  
-	if ( ARGV.length == 2 )
-		options['searchString'] = ARGV[1]
-		puts "Searching package descriptions for : " + options['searchString']
+	when "show-build"  
+		if ( ARGV.length == 2 )
+			options['package'] = ARGV[1]
+			puts "Display build log for package : " + options['package']
 		else
 			show.usage
 			exit
 		end
 
-    when "show-details"  
-	if ( ARGV.length == 2 )
-		options['package'] = ARGV[1]
-		puts "Display details for package : " + options['package']
+
+	when "show-depends"  
+		if ( ARGV.length == 2 )
+			options['package'] = ARGV[1]
+			puts "Display dependency tree for package : " + options['package']
+		else
+			show.usage
+			exit
+		end
+	
+	when "show-files"  
+		if ( ARGV.length == 2 )
+			options['package'] = ARGV[1]
+			puts "Display installed files from package : " + options['package']
 		else
 			show.usage
 			exit
 		end
 
-    when "show-build"  
-	if ( ARGV.length == 2 )
-		options['package'] = ARGV[1]
-		puts "Display build log for package : " + options['package']
+	when "show-owner"  
+		if ( ARGV.length == 2 )
+			options['fileName'] = ARGV[1]
+			puts "Display owning package for file : " + options['fileName']
 		else
 			show.usage
 			exit
 		end
+	
+	when "show-installed"  
+		puts "Display all installed packages."
+	
+	when "show-frozen"  
+		puts "Display all packages frozen at current version."
+	
+	when "show-untracked"  
+		puts "Display all files on system not tracked by AbTLinux."
 
-
-    when "show-depends"  
-	if ( ARGV.length == 2 )
-		options['package'] = ARGV[1]
-		puts "Display dependency tree for package : " + options['package']
-		else
-			show.usage
-			exit
-		end
-
-    when "show-files"  
-	if ( ARGV.length == 2 )
-		options['package'] = ARGV[1]
-		puts "Display installed files from package : " + options['package']
-		else
-			show.usage
-			exit
-		end
-
-    when "show-owner"  
-	if ( ARGV.length == 2 )
-		options['fileName'] = ARGV[1]
-		puts "Display owning package for file : " + options['fileName']
-		else
-			show.usage
-			exit
-		end
-
-    when "show-installed"  
-	puts "Display all installed packages."
-
-    when "show-frozen"  
-	puts "Display all packages frozen at current version."
-
-    when "show-untracked"  
-	puts "Display all files on system not tracked by AbTLinux."
-
-    when "show-journal"  
-	puts "Display system log with AbTLinux activity."
-
-    when "show-iqueue"  
-	puts "Display contents of install queue."
-
-    when "show-patches"  
-	puts "Display currently available patches for installed package tree."
-
-    when "show-updates"  
-	puts "Display package listing with available update versions."
-
-    when "html"
-    puts "Generate HTML page from installed packages:"
-    puts "  (package name with hyperlink to package website and version installed)"
-
-    when "news", "-n"
-    puts "Display AbTLinux website newsfeed."
+	when "show-journal"  
+		puts "Display system log with AbTLinux activity."
+	
+	when "show-iqueue"  
+		puts "Display contents of install queue."
+	
+	when "show-patches"  
+		puts "Display currently available patches for installed package tree."
+	
+	when "show-updates"  
+		puts "Display package listing with available update versions."
+	
+	when "html"
+	  puts "Generate HTML page from installed packages:"
+	  puts "  (package name with hyperlink to package website and version installed)"
+	
+	when "news", "-n"
+  puts "Display AbTLinux website newsfeed."
             
-when "download", "-d"  
-	if ( ARGV.length == 2 )
-		options['package'] = ARGV[1]
-		puts "Retrieve sources for package : " + options['package']
-	else
-		show.usage
-		exit
-	end
+	when "download", "-d"  
+		if ( ARGV.length == 2 )
+			options['package'] = ARGV[1]
+			puts "Retrieve sources for package : " + options['package']
+		else
+			show.usage
+			exit
+		end
+		
+	when "update", "-u"  
+		if ( ARGV.length == 2 )
+			options['updateItem'] = ARGV[1]
+			puts "Updating this item (either package or a package tree : " + options['updateItem']
+		else
+			show.usage
+			exit
+		end
+
+	when "purge-src"
+	  puts "Remove source caches for packages no longer installed."
 	
-when "update", "-u"  
-	if ( ARGV.length == 2 )
-		options['updateItem'] = ARGV[1]
-		puts "Updating this item (either package or a package tree : " + options['updateItem']
+	when "purge-logs"
+	  puts "Remove log files for packages no longer installed."
+	
+	when "verify-files"  
+		if ( ARGV.length == 2 )
+			options['package'] = ARGV[1]
+			puts "Installed files verified for package : " + options['package']
 		else
 			show.usage
 			exit
 		end
-
-    when "purge-src"
-    puts "Remove source caches for packages no longer installed."
-
-    when "purge-logs"
-    puts "Remove log files for packages no longer installed."
-
-    when "verify-files"  
-	if ( ARGV.length == 2 )
-		options['package'] = ARGV[1]
-		puts "Installed files verified for package : " + options['package']
-		else
-			show.usage
-			exit
-		end
-
-    when "verify-symlinks"  
-	if ( ARGV.length == 2 )
-		options['package'] = ARGV[1]
-		puts "Symlinks verified for package : " + options['package']
-		else
-			show.usage
-			exit
-		end
-
-    when "verify-deps"  
-	if ( ARGV.length == 2 )
+	
+	when "verify-symlinks"  
+		if ( ARGV.length == 2 )
 		options['package'] = ARGV[1]
 		puts "Symlinks verified for package : " + options['package']
 	else
 		show.usage
 		exit
 	end
-    
-when "verify-integrity"  
-	if ( ARGV.length == 2 )
-		options['package'] = ARGV[1]
-		puts "Verifiy the integrity of installed files for package : " + options['package']
+
+	when "verify-deps"  
+		if ( ARGV.length == 2 )
+			options['package'] = ARGV[1]
+			puts "Symlinks verified for package : " + options['package']
 		else
 			show.usage
 			exit
 		end
-
-    when "fix"  
-	if ( ARGV.length == 2 )
-		options['package'] = ARGV[1]
-		puts "Package : " + options['package'] + " is verified and checked if needed."
-		else
-			show.usage
-			exit
-		end
-
-    when "build-location"  
-	if ( ARGV.length == 2 )
-		options['buildHost'] = ARGV[1]
-		puts "Sets global location for retrieving cached build packages to : " + options['buildHost']
-		else
-			show.usage
-			exit
-		end
-
-    when "package-repo"
-
-    # sort out that we have enough args.
-    case ARGV.length
-    
-    # add or remove called.
-    when 3
-    	options['repoAction'] = ARGV[1]
-		options['repoUri'] = ARGV[2]
-    
-    # list called.
-    when 2
-        if ( ARGV[1] == "list" )
-            options['repoAction'] = ARGV[1]
-        else
-            show.usage
-            exit
-        end
-        
-    else
-        show.usage
-        exit		    
-	end
-	
-	# hook location based on action.
-	case options['repoAction']
-		
-	when "add"
-	    puts "Adding package repository : " + options['repoUri']
-		
-	when "remove"
-	    puts "Remove package repository : " + options['repoUri']
-		
-	when "list"
-	    puts "Display listing of package repositories."
 	    
-	else
-	    show.usage
-	    exit  
-	end
-end # case
+	when "verify-integrity"  
+		if ( ARGV.length == 2 )
+			options['package'] = ARGV[1]
+			puts "Verifiy the integrity of installed files for package : " + options['package']
+		else
+			show.usage
+			exit
+		end
+
+	when "fix"  
+		if ( ARGV.length == 2 )
+			options['package'] = ARGV[1]
+			puts "Package : " + options['package'] + " is verified and checked if needed."
+		else
+			show.usage
+			exit
+		end
+	
+	when "build-location"  
+		if ( ARGV.length == 2 )
+			options['buildHost'] = ARGV[1]
+			puts "Sets global location for retrieving cached build packages to : " + options['buildHost']
+		else
+			show.usage
+			exit
+		end
+
+	when "package-repo"
+	  # sort out that we have enough args.
+	  case ARGV.length
+	
+			# add or remove called.
+	    when 3
+	    	options['repoAction'] = ARGV[1]
+				options['repoUri'] = ARGV[2]
+	    
+	    # list called.
+	    when 2
+	      if ( ARGV[1] == "list" )
+					options['repoAction'] = ARGV[1]
+	      else
+	        show.usage
+	        exit
+	      end
+	
+	    else
+	      show.usage
+	      exit		    
+		end # case ARGV.length.
+		
+		# hook location based on action.
+		case options['repoAction']
+	
+			when "add"
+		    puts "Adding package repository : " + options['repoUri']
+			
+			when "remove"
+		    puts "Remove package repository : " + options['repoUri']
+		
+			when "list"
+	    	puts "Display listing of package repositories."
+	    
+			else
+	    	show.usage
+	    	exit  
+		end # case repoAction.
+end # case ARGV[0].
