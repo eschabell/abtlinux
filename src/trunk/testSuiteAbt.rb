5d63c239b3ab8ef2b32d809b28347fc1834df7d2
eschabell <eschabell@16b9798a-5803-0410-9976-a82bee9f84b4>
2006-11-15 14:12:50 +0000

    Ensure that test suite runs as root too.

--- src/trunk/testSuiteAbt.rb
+++ src/trunk/testSuiteAbt.rb
@@ -1,4 +1,10 @@
-#!/usr/bin/ruby -w
+#!/usr/bin/ruby -wI./packages
+
+if ( Process.uid != 0 )
+	puts "Enter root password:"
+	system( 'su -c ./testSuiteAbt.rb root' )
+	exit
+end
 
 require 'test/unit'
 require 'TestAbtDepEngine'
