51f162f230b5d74327b87974132c3886d19c826f
eschabell <eschabell@16b9798a-5803-0410-9976-a82bee9f84b4>
2007-02-25 21:26:06 +0000

    Added needed required libs to get tests to run solo. Now they are only failing on the known problem of being unable to find the path to packages.
    Refactored tests to make use of valid Ipc.rb package.
    
    Passing pre, configure, build, preinsall and post tests.

--- src/trunk/TestAbtPackage.rb
+++ src/trunk/TestAbtPackage.rb
@@ -2,7 +2,10 @@
 
 require 'test/unit/testcase'
 require 'test/unit/autorunner'
+require 'abtconfig'
 require 'AbtPackage'
+require 'AbtDownloadManager'
+require 'packages/ipc'
 
 ##
 # TestAbtPackage.rb 
@@ -30,84 +33,71 @@ require 'AbtPackage'
 ##
 class TestAbtPackage < Test::Unit::TestCase
 
-  $name     = "Fortune"
-  $version  = "mod-9708"
-  $srcDir   = "#{$name.downcase}-#{$version}"
+  $nameTest     = "Ipc"
+  $versionTest  = "1.4"
+  $srcDirTest   = "#{$nameTest.downcase}-#{$versionTest}"
 
   $data = {
-    'name'              => "Fortune",
-    'execName'          => $name.downcase,
-    'version'           => $version,
-    'srcDir'            => $srcDir,
-    'homepage'          => "http://www.ibiblio.org/pub/Linux/games/amusements/#{$name.downcase}/",
-    'srcUrl'            => "http://www.ibiblio.org/pub/Linux/games/amusements/#{$srcDir}.tar.gz",
-    'dependsOn'         => "none",
-    'reliesOn'          => "none",
-    'optionalDO'        => "none",
-    'optionalRO'        => "none",
-    'hashCheck'         => "sha512:80c5b71d84eeb3092b2dfe483f0dad8ed42e2ef",
-    'patches'           => "http://patches.abtlinux.org/#{$srcDir}-patches-1.tar.gz",
-    'patchesHashCheck'  => "sha512:80c5b71d84eeb3092b2dfe483f0dad8ed42",
-    'mirrorPath'        => "http://mirror.abtlinux.org/#{$srcDir}.tar.gz",
-    'license'           => "GPL",
-    'description'       => "Prints a random, hopefully interesting, adage." 
+    'name'              => $nameTest,
+    'execName'          => $nameTest.downcase,
+    'version'           => $versionTest,
+    'srcDir'            => $srcDirTest,
+    'homepage'          => "http://isotopatcalc.sourceforge.net/"
   }
 
   ##
   # setup method for testing AbtPackage.
   ## 
   def setup
-    @fortune = AbtPackage.new( $data )
+    @sw = Ipc.new
   end
 
   ##
   # Test method for 'AbtPackage.testDetails()'
   ## 
   def testDetails
-    assert_equal( $data['name'], @fortune.details['Package name'], "testDetails()" )
-    assert_equal( $data['execName'], @fortune.details['Executable'], "testDetails()" )
-    assert_equal( $data['version'], @fortune.details['Version'], "testDetails()" )
-    assert_equal( $data['srcDir'], @fortune.details['Source location'], "testDetails()" )
-    assert_equal( $data['homepage'], @fortune.details['Homepage'], "testDetails()" )
-    assert_equal( $data['srcUrl'], @fortune.details['Source uri'], "testDetails()" )
-    assert_equal( $data['dependsOn'], @fortune.details['Depends On'], "testDetails()" )
-    assert_equal( $data['reliesOn'], @fortune.details['Relies On'], "testDetails()" )
-    assert_equal( $data['optionalDO'], @fortune.details['Optional DO'], "testDetails()" )
-    assert_equal( $data['optionalRO'], @fortune.details['Optional RO'], "testDetails()" )
-    assert_equal( $data['hashCheck'], @fortune.details['Security hash'], "testDetails()" )
-    assert_equal( $data['patches'], @fortune.details['Patches'], "testDetails()" )
-    assert_equal( $data['patchesHashCheck'], @fortune.details['Patches hash'], "testDetails()" )
-    assert_equal( $data['mirrorPath'], @fortune.details['Mirror'], "testDetails()" )
-    assert_equal( $data['license'], @fortune.details['License'], "testDetails()" )
-    assert_equal( $data['description'], @fortune.details['Description'], "testDetails()" )
+    assert_equal( $data['name'], @sw.details['Package name'], "testDetails()" )
+    assert_equal( $data['execName'], @sw.details['Executable'], "testDetails()" )
+    assert_equal( $data['version'], @sw.details['Version'], "testDetails()" )
+    assert_equal( $data['srcDir'], @sw.details['Source location'], "testDetails()" )
+    assert_equal( $data['homepage'], @sw.details['Homepage'], "testDetails()" )
   end
   
   ##
   # Test method for 'AbtPackage.testPre()'
   ## 
   def testPre
-    assert( @fortune.pre(), "testPre()" )
+    assert( @sw.pre(), "testPre()" )
   end
 
   ##
   # Test method for 'AbtPackage.testConfigure()'
   ##
   def testConfigure
-    assert_equal( true, true, "testConfigure()" )
+    if ( !@sw.pre() ) 
+      assert_equals( true, false, "testConfigure()" )
+    end
+    assert( @sw.configure(), "testConfigure()" )
   end
 
   ##
   # Test method for 'AbtPackage.testBuild()'
   ##
   def testBuild
-    assert_equal( false, true, "testBuild()" )
+    if ( !@sw.pre() ) 
+      assert_equals( true, false, "testConfigure()" )
+    end
+    if ( !@sw.configure() ) 
+      assert_equals( true, false, "testConfigure()" )
+    end
+    assert( @sw.build(), "testBuild()" )
   end
 
   ##
   # Test method for 'AbtPackage.testPreinstall()'
   ##
   def testPreinstall
-    assert_equal( false, true, "testPreinstall()" )
+    assert( @sw.preinstall(), "testPreinstall()" )
   end
 
   ##
@@ -121,7 +111,7 @@ class TestAbtPackage < Test::Unit::TestCase
   # Test method for 'AbtPackage.testPost()'
   ##
   def testPost
-    assert_equal( false, true, "testPost()" )
+    assert( @sw.post(), "testPost()" )
   end
 
 end
