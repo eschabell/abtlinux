296e0af1056e7cf0439605378c53c223134ba482
eschabell <eschabell@16b9798a-5803-0410-9976-a82bee9f84b4>
2007-07-24 13:35:55 +0000

    Refactored for better test coverage. 6 tests still failing.

--- src/trunk/testabtsystemmanager.rb
+++ src/trunk/testabtsystemmanager.rb
@@ -34,7 +34,11 @@ class TestAbtSystemManager < Test::Unit::TestCase
   # setup method for testing AbtSystemManager.
   ## 
   def setup
-    @sys = AbtSystemManager.new()
+    @system = AbtSystemManager.new
+    @manager = AbtPackageManager.new
+    
+    # ensure tarball availabe without downloading.
+    FileUtils.cp( "#{$PACKAGE_PATH}/ipc-1.4.tar.gz", "#{$SOURCES_REPOSITORY}", :verbose => true ) if !File.exist?( "#{$SOURCES_REPOSITORY}/ipc-1.4.tar.gz" ) 
   end
   
   ##
@@ -47,63 +51,84 @@ class TestAbtSystemManager < Test::Unit::TestCase
   # Test method for 'AbtSystemManager.test_cleanup_package_sources()'
   ## 
   def test_cleanup_package_sources
-    assert( @sys.cleanup_package_sources(), "test_cleanup_package_sources()" )
+    # remove test package so that sources can be removed.
+    if @system.package_installed( "ipc" )
+      @manager.remove_package( "ipc" )
+    end
+    
+    # ensure tarball availabe without downloading.
+    FileUtils.cp( "#{$PACKAGE_PATH}/ipc-1.4.tar.gz", "#{$SOURCES_REPOSITORY}", :verbose => true ) if !File.exist?( "#{$SOURCES_REPOSITORY}/ipc-1.4.tar.gz" ) 
+    
+    assert( @system.cleanup_package_sources(), "test_cleanup_package_sources()" )
   end
     
   ##
   # Test method for 'AbtSystemManager.test_verify_installed_files()'
   ## 
   def test_verify_installed_files
-    assert( @sys.verify_installed_files( "ipc" ), "test_verify_installed_files()" )
+    # ensure test package installed.
+    if !@system.package_installed( "ipc" )
+      @manager.install_package( "ipc" )
+    end
+    
+    assert( @system.verify_installed_files( "ipc" ), "test_verify_installed_files()" )
   end
   
   ##
   # Test method for 'AbtSystemManager.test_verify_symlinks()'
   ## 
   def test_verify_symlinks
-    assert( @sys.verify_symlinks( "dummy" ), "test_verify_symlinks()" )
+    assert( @system.verify_symlinks( "dummy" ), "test_verify_symlinks()" )
   end
   
   ##
   # Test method for 'AbtSystemManager.test_verify_package_depends()'
   ## 
   def test_verify_package_depends
-    assert( @sys.verify_package_depends( "dummy" ), "test_verify_package_depends()" )
+    assert( @system.verify_package_depends( "dummy" ), "test_verify_package_depends()" )
   end
   
   ##
   # Test method for 'AbtSystemManager.test_verify_package_integrity()'
   ## 
   def test_verify_package_integrity
-    assert( @sys.verify_package_integrity( "dummy" ), "test_verify_package_integrity()" )
+    if !@system.package_installed( "ipc" )
+      @manager.install_package( "ipc" )
+    end
+    
+    assert( @system.verify_package_integrity( "ipc" ), "test_verify_package_integrity()" )
   end
   
   ##
   # Test method for 'AbtSystemManager.test_fix_package()'
   ## 
   def test_fix_package
-    assert( @sys.fix_package( "dummy" ), "test_fix_package()" )
+    assert( @system.fix_package( "dummy" ), "test_fix_package()" )
   end
   
   ##
   # Test method for 'AbtSystemManager.test_set_central_repo()'
   ## 
   def test_set_central_repo
-    assert( @sys.set_central_repo( "http://localhost" ), "test_set_central_repo()" )
+    assert( @system.set_central_repo( "http://localhost" ), "test_set_central_repo()" )
   end
   
   ##
   # Test method for 'AbtSystemManager.test_set_package_tree_location()'
   ## 
   def test_set_package_tree_location
-    assert( @sys.set_package_tree_location( "/var/lib/ericsPackages" ), "test_set_package_tree_location()" )
+    assert( @system.set_package_tree_location( "/var/lib/ericsPackages" ), "test_set_package_tree_location()" )
   end
     
   ##
   # Test method for 'AbtSystemManager.test_package_installed()'
   ## 
   def test_package_installed
-    assert( @sys.package_installed( "ipc" ), "test_package_installed()" )
+    if !@system.package_installed( "ipc" )
+      @manager.install_package( "ipc" )
+    end
+    
+    assert( @system.package_installed( "ipc" ), "test_package_installed()" )
   end
   
 end
