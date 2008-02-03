421dd878f2b9a328f7406667dbe99cac0f362ebc
eschabell <eschabell@16b9798a-5803-0410-9976-a82bee9f84b4>
2008-02-03 09:05:44 +0000

    Added first setup for a simple package generation script.

---
+++ src/trunk/new_abt_package.rb
@@ -0,0 +1,144 @@
+#!/usr/bin/ruby -w
+
+##
+# new_abt_package.rb
+#
+# Creates a new abt package using a simple standard template and user queries.
+#
+# Created by Eric D. Schabell <erics@abtlinux.org>
+# Copyright 2006, GPL.
+#
+# This file is part of AbTLinux.
+#
+# AbTLinux is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# AbTLinux is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# AbTLinux; if not, write to the Free Software Foundation, Inc., 51 Franklin
+# St, Fifth Floor, Boston, MA 02110-1301  USA
+##
+class NewAbtPackage
+  
+  # get name of package.
+  puts "What is the packages name?"
+  packageName = gets
+  
+  # get version.
+  
+  
+  # get source directory name (after unpacking).
+  
+  # get source file name.
+
+  # get website / homepage.
+  
+  # get source url.
+  
+  # get dependsOn.
+  
+  # get reliesOn.
+  
+  # get optional dependsOn.
+  
+  # get optional reliesOn.
+  
+  # get hash check (get file location and run Digest::SHA1.hexdigest(path)).
+  
+  # get license field.
+  
+  # get descriptions text.
+  
+  
+  ##
+  # Generate pacakge file.
+  ##
+  
+  print <<EOF
+  
+  #!/usr/bin/ruby -w
+
+  $LOAD_PATH.unshift '/var/lib/abt/'
+  
+  require 'abtpackage'
+  
+  ##
+  # #{packageName.downcase}.rb 
+  #
+  # #{packageName.upcase} package.
+  #
+  # Created by Eric D. Schabell <erics@abtlinux.org>
+  # Copyright 2008, GPL.
+  #
+  # This file is part of AbTLinux.
+  #
+  # AbTLinux is free software; you can redistribute it and/or modify
+  # it under the terms of the GNU General Public License as published by
+  # the Free Software Foundation; either version 2 of the License, or
+  # (at your option) any later version.
+  #
+  # AbTLinux is distributed in the hope that it will be useful, but WITHOUT ANY
+  # WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+  # FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+  # details.
+  #                                   
+  # You should have received a copy of the GNU General Public License along with
+  # AbTLinux; if not, write to the Free Software Foundation, Inc., 51 Franklin
+  # St, Fifth Floor, Boston, MA 02110-1301  USA
+  ##
+  
+  class #{packageName.upcase} < AbtPackage
+    
+  protected
+    
+  private
+    
+    $name         = "#{packageName.upcase}"
+    $version      = "2.5.1"
+    $srcDir       = "#{$name.downcase}-#{$version}"
+    $srcFile      = "#{$srcDir}.tar.bz2"
+    $packageData  = {
+      'name'              => $name,
+      'execName'          => $name.downcase,
+      'version'           => $version,
+      'srcDir'            => $srcDir,
+      'homepage'          => "http://www.gnu.org/software/grep/",
+      'srcUrl'            => "ftp://ftp.nluug.nl/pub/gnu/grep/#{$srcFile}",
+      'dependsOn'         => "",
+      'reliesOn'          => "",
+      'optionalDO'        => "",
+      'optionalRO'        => "",
+      'hashCheck'         => "82858cc631a7f1ef55597aff661474bef481d4a2",
+      'patches'           => "",
+      'patchesHashCheck'  => "",
+      'mirrorPath'        => "",
+      'license'           => "GPL2",
+      'description'       => 
+      "The grep command searches one or more input files for lines containing 
+        a match to a specified pattern. By default, grep prints the matching lines."
+    }
+  
+    
+  public
+  
+    ##
+    # Constructor for an AbtPackage, requires all the packge details.
+    #
+    # <b>PARAM</b> <i>Hash</i> - hash containing all pacakge data.
+    #
+    ##
+    def initialize()
+        super($packageData)
+    end  
+  end
+
+  EOF
+  
+  
+  end
